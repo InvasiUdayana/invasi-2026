3d6ceb3c6f5641e929e82eda3734effd8cd6b76a
InvasiUdayana <136177073+InvasiUdayana@users.noreply.github.com>
2026-08-29 23:37:37 +0800

    Fix route case sensitivity for registration path

--- invasi-2026/next.config.mjs
+++ invasi-2026/next.config.mjs
@@ -173,7 +173,7 @@ const nextConfig = {
       },
       //TWN INVASI 2026
       {
-        source: "/pendaftaranTWNinvasi2026",
+        source: "/PendaftaranTWNinvasi2026",
         destination: "https://forms.gle/W1ksF6hk7kecEjUt9",
         permanent: true,
       },
